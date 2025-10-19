81be472fee173d601f53f01d9d90b4073a3669ed
Cncspt <95188174+TheWooffles@users.noreply.github.com>
2025-10-19 19:22:34 -0400

    F

--- Venex.lua
+++ Venex.lua
@@ -172,7 +172,7 @@ LeftGroupBox:AddInput('MyTextbox', {
 Options.MyTextbox:OnChanged(function()
     print('Text updated. New text:', Options.MyTextbox.Value)
 end)
-Library.AccentColor = Color3.fromRGB(255, 85, 255);
+Library.AccentColor:SetValueRGB(255, 85, 255);
 
 -- Groupbox:AddDropdown
 -- Arguments: Idx, Info
