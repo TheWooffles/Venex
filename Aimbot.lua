15c796f127a5d9a4891c07fd4e419f7484568063
Cncspt <95188174+TheWooffles@users.noreply.github.com>
2025-10-25 11:13:13 -0400

    Fix

---
+++ Aimbot.lua
@@ -0,0 +1,91 @@
+--// Services & Variables
+local Players          = game:GetService("Players")
+local LocalPlayer      = Players.LocalPlayer
+local Camera           = workspace.CurrentCamera
+local RunService       = game:GetService("RunService")
+local UserInputService = game:GetService("UserInputService")
+local TweenService     = game:GetService("TweenService")
+local Holding          = false
+
+--// Config
+_G.AimbotEnabled = true
+_G.TeamCheck     = true
+_G.WallCheck     = true
+_G.AimPart       = "Head"
+_G.Smoothness    = 0.2
+_G.Radius        = 30
+_G.FovColor      = Color3.fromRGB(255,255,255)
+_G.FovVisible    = true
+
+--// Gui
+local FovCircle     = Drawing.new("Circle")
+FovCircle.Position  = Vector2.new(Camera.ViewportSize.X / 2, Camera.ViewportSize.Y / 2)
+FovCircle.Radius    = _G.Radius
+FovCircle.Color     = _G.FovColor
+FovCircle.Visible   = _G.FovVisible
+FovCircle.Thickness = 1
+
+--// Logic & Main Loop
+local function GetClosestPlayer()
+    local MaximumDistance _G.Radius
+    local Target = nil
+
+    for _, v in next, Players:GetPlayers() do
+        if v.Name ~= LocalPlayer.Name then
+            if _G.TeamCheck == true then
+                if v.Team ~= LocalPlayer.Team then
+                    if v.Character ~= nil then
+                        if v.Character:FindFirstChild("HumanoidRootPart") ~= nil then
+                            if v.Character:FindFirstChild("Humanoid") ~= nil and v.Character:FindFirstChild("Humanoid").Health ~= 0 then
+                                local ScreenPoint = Camera:WorldToViewportPoint(v.Character:WaitForChild("HumanoidRootPart", math.huge).Position)
+                                local VectorDistance = (Vector2.new(UserInputService:GetMouseLocation().X, UserInputService:GetMouseLocation().Y) - Vector2.new(ScreenPoint.X, ScreenPoint.Y)).Magnitude
+
+                                if VectorDistance < MaximumDistance then
+                                    Target = v
+                                end
+                            end
+                        end
+                    end
+                end
+            else
+                if v.Character ~= nil then
+                        if v.Character:FindFirstChild("HumanoidRootPart") ~= nil then
+                            if v.Character:FindFirstChild("Humanoid") ~= nil and v.Character:FindFirstChild("Humanoid").Health ~= 0 then
+                                local ScreenPoint = Camera:WorldToViewportPoint(v.Character:WaitForChild("HumanoidRootPart", math.huge).Position)
+                                local VectorDistance = (Vector2.new(UserInputService:GetMouseLocation().X, UserInputService:GetMouseLocation().Y) - Vector2.new(ScreenPoint.X, ScreenPoint.Y)).Magnitude
+
+                                if VectorDistance < MaximumDistance then
+                                    Target = v
+                                end
+                            end
+                        end
+                    end
+                end
+            end
+        end
+    end
+    return Target
+end
+
+UserInputService.InputBegan:Connect(function(Input)
+    if Input.UserInputType == Enum.UserInputType.MouseButton2 then
+        Holding = true
+    end
+end)
+
+UserInputService.InputEnded:Connect(function(Input)
+    if Input.UserInputType == Enum.UserInputType.MouseButton2 then
+        Holding = false
+    end
+end)
+
+RunService.RenderStepped:Connect(function()
+    FovCircle.Position = Vector2.new(UserInputService:GetMouseLocation().X, UserInputService:GetMouseLocation().Y)
+    FovCircle.Radius   = _G.Radius
+    FovCircle.Color    = _G.FovColor
+
+    if Holding == true and _G.AimbotEnabled == true then 
+        local cf = CFrame.lookAt(Camera.CFrame.Position, GetClosestPlayer().Character[_G.AimPart].Position)
+        Camera.CFrame:Lerp(cf, math.clamp(_G.Smoothness))
+    end
+end)
